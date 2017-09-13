a217fc4f77ba9577488718c037d0013c1bdf6c51
Kalyankumar25 <31845917+Kalyankumar25@users.noreply.github.com>
2017-09-13 13:19:16 +0530

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 public class Main {
 
 	public static void main(String[] args) {
-						
-			int []a = new int(10);
-			Student a[0] = new Student(25,"Varun",05/05/1995,75.2,"Hyderabad");
-			Student a[1] = new Student(35,"Arun",04/03/1996,82.1,"pune");
-			Student a[2] = new Student(45,"Babu",06/08/1995,68.9,"Vizag");
-			
-			void display(){
-				
-				System.out.println(a[0],a[1],a[2]);
-				}
-			
+			StudentGroup s1 = new StudentGroup();
+			s1.setStudent();
+			StudentGroup s2 = new StudentGroup();
+			s2.setStudent();
+			StudentGroup s3 = new StudentGroup():
+			s3.setStudent();
+ 			
 	
 	
 
